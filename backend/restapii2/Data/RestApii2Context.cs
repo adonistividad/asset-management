d92f067421cf0063381007f70ef3c2a2ebac2543
Adonis V. Tividad <avtividad@yahoo.com>
2019-05-22 22:17:40 +0400

    Added backend module

---
+++ backend/restapii2/Data/RestApii2Context.cs
@@ -0,0 +1,21 @@
+﻿using restapii2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace restapii2.Context
+{
+    public class RestApii2Context : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        public DbSet<tblAsset> Assets { get; set; }
+
+
+    }
+}
---
+++ backend/restapii2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="restapii2.WebApiApplication" Language="C#" %>
