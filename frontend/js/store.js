83983c65a0189dba3da97e5fc0c0a9692429e423
Adonis V. Tividad <avtividad@yahoo.com>
2019-05-22 21:55:21 +0400

    Updated store.js, replaced url_current from 'http://localhost:33000/' to 'https://restapii.xyz/'

--- frontend/js/store.js
+++ frontend/js/store.js
@@ -1,5 +1,5 @@
-//let url_current="https://restapii.xyz/";
-let url_current = "http://localhost:31000/";
+let url_current="https://restapii.xyz/";
+// let url_current = "http://localhost:33000/";
 
 const store = new Vuex.Store({
     state: {
@@ -27,7 +27,7 @@ const store = new Vuex.Store({
     },
     actions: {
         async fetchAssets(context) {
-            /*
+            
             url = store.state.api + '/assets';
             context.commit('SET_LOADING_STATUS', 'loading')
             await axios.get(url)
@@ -36,13 +36,15 @@ const store = new Vuex.Store({
                     console.log('response.data.assets', response.data.assets)
                     context.commit('SET_ASSETS', response.data.assets)
                 })
-                */
-               let assets = [{ "assetId": 1, "assetLabel": "Asset 1", "assetDescription": "Description 1", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "John Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
+                
+              /*
+                let assets = [{ "assetId": 1, "assetLabel": "Asset 1", "assetDescription": "Description 1", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "John Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
                { "assetId": 2, "assetLabel": "Asset 2", "assetDescription": "Description 2", "assetType": "Mobile", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:56.497" },
                { "assetId": 3, "assetLabel": "Asset 3", "assetDescription": "Description 3", "assetType": "Laptop", "assetStatus": "In-use", "purchaseDate": null, "currentOwner": "Jane Doe", "deleted": false, "dateCreated": null, "lastUpdated": null },
                { "assetId": 4, "assetLabel": "Asset 4", "assetDescription": "Description 4", "assetType": "Tablet", "assetStatus": "In-repair", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:14:51.49" },
                { "assetId": 5, "assetLabel": "Asset 5", "assetDescription": "Description 5", "assetType": "Tablet", "assetStatus": "Available", "purchaseDate": null, "currentOwner": "Don", "deleted": false, "dateCreated": null, "lastUpdated": "2019-05-22T01:06:13.9" },];
                 context.commit('SET_ASSETS', assets);
+                */
         },
         async assetDelete(context, object) {
             url = store.state.api + '/assets/'+ object.assetId ;
